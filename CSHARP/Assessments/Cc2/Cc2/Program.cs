0bb82d942c93be20d048c6557aef511258500d23
swapnat <swapnat@DESKTOP-6UL3LF0>
2024-06-26 17:10:37 +0530

    ss

---
+++ CSHARP/Assessments/Cc2/Cc2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cc2
+{
+
+    class Product
+    {
+        public int ProductId;
+        public string ProductName;
+        public decimal Price;
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+           
+            List<Product> products = new List<Product>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter details for product {i + 1}:");
+                Product product = new Product();
+
+                Console.Write("Product Name: ");
+                product.ProductName = Console.ReadLine();
+
+                Console.Write("Price: ");
+                product.Price = Convert.ToDecimal(Console.ReadLine());
+
+                product.ProductId = i + 1;
+                products.Add(product);
+            }
+            for (int i = 0; i < products.Count - 1; i++)
+            {
+                for (int j = i + 1; j < products.Count; j++)
+                {
+                    if (products[i].Price > products[j].Price)
+                    {
+                        Product temp = products[i];
+                        products[i] = products[j];
+                        products[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted Products by their Price:");
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}, Name: {product.ProductName}, Price: {product.Price:C}");
+            }
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSHARP/Assessments/Cc2/Cc2/Properties/Exception.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cc2.Properties
+{
+    class Exception
+    {
+        static void Main()
+        {
+            try
+            {
+
+                Console.Write("Enter a number: ");
+                int number = int.Parse(Console.ReadLine());
+                CheckIfNegative(number);
+                
+            } 
+            catch (FormatException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}. Please enter a valid integer.");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+                
+            }
+            Console.Read();
+        }
+        static void CheckIfNegative(int number)
+        {
+            if (number < 0)
+            {
+                Console.WriteLine("Negative numbers are not allowed.");
+            }
+        }
+    }
+
+}
+
