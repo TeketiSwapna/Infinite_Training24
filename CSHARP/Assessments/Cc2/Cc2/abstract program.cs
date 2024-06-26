6ae3e8aaccb0bd645f3e79a0934370f70dd58324
swapnat <swapnat@DESKTOP-6UL3LF0>
2024-06-26 17:17:17 +0530

    ss

---
+++ CSHARP/Assessments/Cc2/Cc2/abstract program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+namespace Cc2
+{
+    public abstract class Student
+    {
+
+        public string name;
+        public int studentId;
+        public double grade;
+
+        // Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int StudentId
+        {
+            get { return studentId; }
+            set { studentId = value; }
+        }
+
+        public double Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+        protected Student(string name, int studentId, double grade)
+        {
+            this.name = name;
+            this.studentId = studentId;
+            this.grade = grade;
+        }
+        public abstract bool IsPassed();
+    }
+    public class Ug : Student
+    {
+        public Ug(string name, int studentId, double grade) : base(name, studentId, grade)
+        {
+        }
+        public override bool IsPassed()
+        {
+            return Grade > 70.0;
+        }
+    }
+    public class Graduate : Student
+    {
+        public Graduate(string name, int studentId, double grade) : base(name, studentId, grade)
+        {
+        }
+        public override bool IsPassed()
+        {
+            return Grade > 80.0;
+        }
+    }
+
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Ug undergrad = new Ug("Divya", 101, 71.0);
+            Console.WriteLine($"{undergrad.Name} with Student ID {undergrad.StudentId} has {(undergrad.IsPassed() ? "passed" : "not passed")}.");
+
+            Graduate grad = new Graduate("Deeksha", 201, 79.0);
+            Console.WriteLine($"{grad.Name} with Student ID {grad.StudentId} has {(grad.IsPassed() ? "passed" : "not passed")}.");
+            Console.ReadKey();
+        }
+    }
+}
