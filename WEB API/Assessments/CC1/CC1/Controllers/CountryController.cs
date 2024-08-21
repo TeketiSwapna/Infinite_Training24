603a21a8c418fa429f5681c25f5b96150a81ddf5
swapnat <swapnat@DESKTOP-6UL3LF0>
2024-08-21 11:25:17 +0530

    CC1

---
+++ WEB API/Assessments/CC1/CC1/Controllers/CountryController.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using System.Web.Mvc;
+using CC1.Models;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace CC1.Controllers
+{
+    public class CountryController : ApiController
+    {
+        private static List<Country> countries = new List<Country>
+        {
+            new Country { ID = 1, CountryName = "India", Capital = "New Delhi" },
+            new Country { ID = 2, CountryName = "USA", Capital = "Washington D.C" },
+            new Country {ID  = 3, CountryName = "Australia", Capital="Canberra"}
+        };
+
+        // GET: api/Country
+        public IHttpActionResult GetCountries()
+        {
+            return Ok(countries);
+        }
+
+        // GET: api/Country/5
+        public IHttpActionResult GetCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        // POST: api/Country
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            // Handle ID assignment correctly
+            country.ID = countries.Any() ? countries.Max(c => c.ID) + 1 : 1;
+
+            countries.Add(country);
+            return CreatedAtRoute("DefaultApi", new { id = country.ID }, country);
+        }
+
+        // PUT: api/Country/5
+        public IHttpActionResult PutCountry(int id, Country country)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existingCountry = countries.FirstOrDefault(c => c.ID == id);
+            if (existingCountry == null)
+            {
+                return NotFound();
+            }
+
+            existingCountry.CountryName = country.CountryName;
+            existingCountry.Capital = country.Capital;
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Country/5
+        public IHttpActionResult DeleteCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            countries.Remove(country);
+            return Ok(country);
+        }
+    }
+}
---
+++ WEB API/Assessments/CC1/CC1/Models/Country.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CC1.Models
+{
+    public class Country
+    {
+        //[Key]
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string Capital { get; set; }
+    }
+}
